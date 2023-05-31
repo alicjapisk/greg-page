1a10ea1b5365913095402549618e75ea8d759d6d
alicjapisk <106108825+alicjapisk@users.noreply.github.com>
2023-05-31 14:58:44 +0200

    added tailwind and next

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header className="text-red-600 text-5xl">
+            Header
+        </header>
+    )
+}
+export default Header;
